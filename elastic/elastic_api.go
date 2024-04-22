54a2f74ed0579954a08767cf31637008e59a8403
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-04-22 13:11:50 +0330

    added a function to search queries in elastic search

---
+++ elastic/elastic_api.go
@@ -0,0 +1,25 @@
+package elastic
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+// SearchQuery receives a query, calls elastic search api to search for documents related tot the query
+func SearchQuery(query string) (map[string]interface{}, error) {
+	encodedQuery := url.QueryEscape(query)
+	url := fmt.Sprintf("%s/movies/_search?q=%s", config.ElasticSearchAddress, encodedQuery)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, _ := io.ReadAll(res.Body)
+	var jsonData map[string]interface{}
+	err = json.Unmarshal(body, &jsonData)
+	return jsonData, err
+}
