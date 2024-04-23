4cadff9f381761d6750864c32e9a21cda36196fe
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-04-23 12:05:10 +0330

    updated tests

--- redis/cache_test.go
+++ redis/cache_test.go
@@ -8,6 +8,8 @@ import (
 
 // TestCache tests functions: SetData and GetData
 func TestCache(t *testing.T) {
+	cache := InitializeRedisClient()
+
 	key := "key"
 	value := map[string]interface{}{
 		"key1": "value1",
@@ -17,28 +19,28 @@ func TestCache(t *testing.T) {
 	}
 
 	t.Run("OK", func(t *testing.T) {
-		err := SetData(key, value, 1*time.Minute)
+		err := cache.SetData(key, value, 1*time.Minute)
 		require.NoError(t, err)
 
-		cachedValue, err := GetData(key)
+		cachedValue, err := cache.GetData(key)
 		require.NoError(t, err)
 
 		require.Equal(t, value, cachedValue)
 	})
 	t.Run("Expired", func(t *testing.T) {
-		err := SetData(key, value, 1*time.Second)
+		err := cache.SetData(key, value, 1*time.Second)
 		require.NoError(t, err)
 
-		_, err = GetData(key)
+		_, err = cache.GetData(key)
 		require.NoError(t, err)
 
 		time.Sleep(1 * time.Second)
 
-		_, err = GetData(key)
+		_, err = cache.GetData(key)
 		require.Error(t, err)
 	})
 	t.Run("NonExistent", func(t *testing.T) {
-		_, err := GetData("non existent key")
+		_, err := cache.GetData("non existent key")
 		require.Error(t, err)
 	})
 }
