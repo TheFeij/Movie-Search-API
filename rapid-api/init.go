e50fd4dc36d48e753bcb9482f336d2d73cf1b1fa
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-04-23 19:25:45 +0330

    updated the init function to exit if config files could not be loaded

--- rapid-api/init.go
+++ rapid-api/init.go
@@ -1,9 +1,11 @@
 // Package rapid_api implements functions to interact with Rapid API's IMDb API.
 package rapid_api
 
+import "log"
+
 // init loads configuration values
 func init() {
 	if err := loadConfig(); err != nil {
-		return
+		log.Fatalln(err)
 	}
 }
--- redis/init.go
+++ redis/init.go
@@ -1,9 +1,11 @@
 // Package redis provides functions to set json data to redis and get json data from it
 package redis
 
+import "log"
+
 // init loads configuration values
 func init() {
 	if err := loadConfig(); err != nil {
-		return
+		log.Fatalln(err)
 	}
 }
