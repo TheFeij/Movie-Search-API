7ecc1e3c3027eb2539f295cb822726b15c383d4f
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-04-23 17:45:35 +0330

    deleted searcher interface

--- searcher/searcher_interface.go
+++
@@ -1,9 +0,0 @@
-// Package searcher contains Searcher Interface
-package searcher
-
-// Searcher  an interface for performing search operations.
-type Searcher interface {
-	// SearchQuery searches for the provided query and returns the result as a map[string]interface{}.
-	// It returns an error if the search operation fails.
-	SearchQuery(query string) (map[string]interface{}, error)
-}
